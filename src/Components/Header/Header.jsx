cc2dadcd391870b8b40d28aa242a4d2382258108
enamahmedshahaz <enamahmedshahaz@gmail.com>
2023-09-13 22:29:57 +0600

    react + tailwind inital setup done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl bg-slate-400'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
